dea042000eca2bc9b449f29fc47fb16c58bd39ad
Arda <116851041+minecarman@users.noreply.github.com>
2024-03-21 12:17:05 +0300

    Update Set2a.hs

--- exercises/Set2a.hs
+++ exercises/Set2a.hs
@@ -1,16 +1,4 @@
--- Exercise set 2:
---  * Guards
---  * Lists
---  * Maybe
---  * Either
---
--- Functions you will need:
---  * head, tail
---  * take, drop
---  * length
---  * null
-
-module Set2a where
+module Set2b where
 
 import Mooc.Todo
 
@@ -18,152 +6,139 @@ import Mooc.Todo
 import Data.List
 
 ------------------------------------------------------------------------------
--- Ex 1: Define the constant years, that is a list of the values 1982,
--- 2004 and 2020 in this order.
+-- Ex 1: compute binomial coefficients using recursion. Binomial
+-- coefficients are defined by the following equations:
+--
+--   B(n,k) = B(n-1,k) + B(n-1,k-1)
+--   B(n,0) = 1
+--   B(0,k) = 0, when k>0
+--
+-- Hint! pattern matching is your friend.
 
-years = [1982, 2004, 2020]
+binomial :: Integer -> Integer -> Integer
+binomial a 0 = 1
+binomial 0 b = 0
+binomial a b = binomial (a-1) b + binomial (a-1) (b-1)
 
 ------------------------------------------------------------------------------
--- Ex 2: define the function takeFinal, which returns the n last
--- elements of the given list.
---
--- If the list is shorter than n, return all elements.
---
--- Hint! remember the take and drop functions.
-
-takeFinal :: Int -> [a] -> [a]
-takeFinal n xs = if length xs < n then xs else drop (length xs - n) xs
-
-------------------------------------------------------------------------------
--- Ex 3: Update an element at a certain index in a list. More
--- precisely, return a list that is identical to the given list except
--- the element at index i is x.
---
--- Note! indexing starts from 0
+-- Ex 2: implement the odd factorial function. Odd factorial is like
+-- factorial, but it only multiplies odd numbers.
 --
 -- Examples:
---   updateAt 0 4 [1,2,3]   ==>  [4,2,3]
---   updateAt 2 0 [4,5,6,7] ==>  [4,5,0,7]
+--   oddFactorial 7 ==> 7*5*3*1 ==> 105
+--   oddFactorial 6 ==> 5*3*1 ==> 15
 
-updateAt :: Int -> a -> [a] -> [a]
-updateAt i x xs = take i xs ++ [x] ++ drop (length xs - i -1) xs
-
-------------------------------------------------------------------------------
--- Ex 4: substring i j s should return the substring of s starting at
--- index i and ending at (right before) index j. Indexes start from 0.
---
--- Remember that strings are lists!
---
--- Examples:
---   substring 2 5 "abcdefgh"  ==>  "cde"
---   substring 2 2 "abcdefgh"  ==>  ""
---   substring 0 4 "abcdefgh"  ==>  "abcd"
-
-substring :: Int -> Int -> String -> String
-substring i j s = take j-i drop i s
+oddFactorial :: Integer -> Integer
+oddFactorial 1 = 1
+oddFactorial n = if odd n then n * oddFactorial (n-2) else oddFactorial n-1
 
 ------------------------------------------------------------------------------
--- Ex 5: check if a string is a palindrome. A palindrome is a string
--- that is the same when read backwards.
+-- Ex 3: implement the Euclidean Algorithm for finding the greatest
+-- common divisor:
 --
--- Hint! There's a really simple solution to this. Don't overthink it!
+-- Given two numbers, a and b,
+-- * if one is zero, return the other number
+-- * if not, subtract the smaller number from the larger one
+-- * replace the larger number with this new number
+-- * repeat
 --
--- Examples:
---   isPalindrome ""         ==>  True
---   isPalindrome "ABBA"     ==>  True
---   isPalindrome "racecar"  ==>  True
---   isPalindrome "AB"       ==>  False
+-- For example,
+--   myGcd 9 12 ==> 3
+-- In this case, the algorithm proceeds like this
+--
+--   a      b
+--
+--   9      12
+--   9      (12-9)
+--   9      3
+--   (9-3)  3
+--   6      3
+--   (6-3)  3
+--   3      3
+--   (3-3)  3
+--   0      3
+--
+-- Background reading:
+-- * https://en.wikipedia.org/wiki/Euclidean_algorithm
 
-isPalindrome :: String -> Bool
-isPalindrome text = text == reverse text
+myGcd :: Integer -> Integer -> Integer
+myGcd 0 b = b
+myGcd a 0 = a 
+myGcd a b = if a <= b then myGcd a (b-a) else myGcd b a 
 
 ------------------------------------------------------------------------------
--- Ex 6: implement the function palindromify that chops a character
--- off the front _and_ back of a string until the result is a
--- palindrome.
+-- Ex 4: Implement the function leftpad which adds space characters
+-- to the start of the string until it is long enough.
 --
 -- Examples:
---   palindromify "ab" ==> ""
---   palindromify "aaay" ==> "aa"
---   palindromify "xabbay" ==> "abba"
---   palindromify "abracacabra" ==> "acaca"
+--   leftpad "foo" 5 ==> "  foo"
+--   leftpad "13" 3 ==> " 13"
+--   leftpad "xxxxx" 3 ==> "xxxxx"
+--
+-- Tips:
+-- * you can combine strings with the ++ operator.
+-- * you can compute the length of a string with the length function
 
-palindromify :: String -> String
-palindromify s = if isPalindrome s then s else palindromify (init (tail s))
+leftpad :: String -> Int -> String
+leftpad str i
+  | len < i = leftpad (" " ++ str) i
+  | otherwise = str
+  where len = length str
 
 ------------------------------------------------------------------------------
--- Ex 7: implement safe integer division, that is, a function that
--- returns a Just result normally, but Nothing if the divisor is zero.
+-- Ex 5: let's make a countdown for a rocket! Given a number, you
+-- should produce a string that says "Ready!", counts down from the
+-- number, and then says "Liftoff!".
 --
--- Remember that integer division can be done with the div function.
+-- For example,
+--   countdown 4 ==> "Ready! 4... 3... 2... 1... Liftoff!"
 --
--- Examples:
---   safeDiv 4 2  ==> Just 2
---   safeDiv 4 0  ==> Nothing
+-- Hints:
+-- * you can combine strings with the ++ operator
+-- * you can use the show function to convert a number into a string
+-- * you'll probably need a recursive helper function
 
-safeDiv :: Integer -> Integer -> Maybe Integer
-safeDiv x y = if y == 0 then Nothing else Just (div x y)
+countdown :: Integer -> String
+countdown = todo
 
 ------------------------------------------------------------------------------
--- Ex 8: implement a function greet that greets a person given a first
--- name and possibly a last name. The last name is represented as a
--- Maybe String value.
+-- Ex 6: implement the function smallestDivisor that returns the
+-- smallest number (greater than 1) that divides the given number evenly.
 --
--- Examples:
---   greet "John" Nothing         ==> "Hello, John!"
---   greet "John" (Just "Smith")  ==> "Hello, John Smith!"
+-- That is, when
+--   smallestDivisor n ==> k
+-- we have
+--   n = t*k
+-- for some t.
+--
+-- Ps. your function doesn't need to work for inputs 0 and 1, but
+-- remember this in the next exercise!
+--
+-- Hint: remember the mod function!
 
-greet :: String -> Maybe String -> String
-greet first last = case last of Nothing -> "Hello," ++ first
-                                Just last -> "Hello," ++ first ++ last
+smallestDivisor :: Integer -> Integer
+smallestDivisor = todo
 
 ------------------------------------------------------------------------------
--- Ex 9: safe list indexing. Define a function safeIndex so that
---   safeIndex xs i
--- gets the element at index i in the list xs. If i is not a valid
--- index, Nothing is returned.
+-- Ex 7: implement a function isPrime that checks if the given number
+-- is a prime number. Use the function smallestDivisor.
 --
--- Examples:
---   safeIndex [True] 1            ==> Nothing
---   safeIndex [10,20,30] 0        ==> Just 10
---   safeIndex [10,20,30] 2        ==> Just 30
---   safeIndex [10,20,30] 3        ==> Nothing
---   safeIndex ["a","b","c"] (-1)  ==> Nothing
+-- Ps. 0 and 1 are not prime numbers
 
-safeIndex :: [a] -> Int -> Maybe a
-safeIndex xs i 
-  | len <= 0 || len < i = Nothing
-  | len >= i = Just (xs !! i)
-  where len = length xs
+isPrime :: Integer -> Bool
+isPrime = todo
 
 ------------------------------------------------------------------------------
--- Ex 10: another variant of safe division. This time you should use
--- Either to return a string error message.
+-- Ex 8: implement a function biggestPrimeAtMost that returns the
+-- biggest prime number that is less than or equal to the given
+-- number. Use the function isPrime you just defined.
+--
+-- You don't need to care about arguments less than 2. Any behaviour
+-- for them is fine.
 --
 -- Examples:
---   eitherDiv 4 2   ==> Right 2
---   eitherDiv 4 0   ==> Left "4/0"
+--   biggestPrimeAtMost 3 ==> 3
+--   biggestPrimeAtMost 10 ==> 7
 
-eitherDiv :: Integer -> Integer -> Either String Integer
-eitherDiv x y
-  | y == 0 = Left (show x ++ "/" ++ show y)
-  | otherwise = Right (div x y)
-
-------------------------------------------------------------------------------
--- Ex 11: implement the function addEithers, which combines two values of type
--- Either String Int into one like this:
---
--- - If both inputs were Ints, sum the Ints
--- - Otherwise, return the first argument that was not an Int
---
--- Hint! Remember pattern matching
---
--- Examples:
---   addEithers (Right 1) (Right 2) ==> Right 3
---   addEithers (Right 1) (Left "fail") ==> Left "fail"
---   addEithers (Left "boom") (Left "fail") ==> Left "boom"
-
-addEithers :: Either String Int -> Either String Int -> Either String Int
-addEithers (Right a) (Right b) = Right (a + b)
-addEithers (Left a) _ = Left a
-addEithers _ (Left b) = Left b
+biggestPrimeAtMost :: Integer -> Integer
+biggestPrimeAtMost = todo
